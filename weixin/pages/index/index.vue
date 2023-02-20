dddb7ecb5755e37874d3a447ce3f3f0aa1e90ff1
笑脸带眼红 <202606540@qq.com>
2023-02-20 12:44:53 +0800

    第一次提交，项目初始化

---
+++ weixin/pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<view class="container">
+		{{ hello }}
+		
+	</view>
+</template>
+
+<script setup>
+import { ref } from "vue";
+	
+	const hello = ref("你好 微信")
+	
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
